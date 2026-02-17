c947b60047ea9bd05a61f1c2758ad7203a24728b
matanatkhalil <khalilmatanat@gmail.com>
2026-02-17 20:46:10 +0400

    fix: fix typo in validate-uuid.js

--- backend/src/middlewares/validate-uuid.js
+++ backend/src/middlewares/validate-uuid.js
@@ -11,3 +11,17 @@ const validateUUID = (paramName) => {
 };
 
 module.exports = { validateUUID };
+
+const { param } = require('express-validator');
+const { validateFields } = require('./validate-fields');
+
+// A middleware helper that validates a specific URL parameter as UUID
+
+const validateUUID = (paramName) => {
+  return [
+    param(paramName, `${paramName} must be a valid UUID`).isUUID(),
+    validateFields, // This trriggers the 400 error if the above fails
+  ];
+};
+
+module.exports = { validateUUID };
